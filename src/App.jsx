7adbeb284e7dcc537784e7a49cdad249d49ab0eb
GBlecher <gilanblecher@gmail.com>
2025-10-30 16:28:59 +0200

    hash route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { GameProvider } from "./context/GameContext";
 import Home from "./pages/Home";
 import GameSelect from "./pages/GameSelect";
